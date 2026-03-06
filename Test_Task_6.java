990669dac10d8527a07466959a05217bc2a352aa
nhajiyev17126 <nhajiyev17126@ada.edu.az>
2026-03-06 09:25:31 +0400

    Fully completed and compiled all codes succesfully!

--- Test_Task_6.java
+++ Test_Task_6.java
@@ -2,24 +2,20 @@ public class Test_Task_6 {
 
     public static void main(String[] args) {
 
-        // create a point
         Point p = new Point(2, 3);
         System.out.print("Point: ");
         p.display();
 
-        // move the point
         p.moveUp();
         p.moveRight();
 
         System.out.print("Point after moving: ");
         p.display();
 
-        // create a circle
         Circle c = new Circle(new Point(5, 5), 10);
         System.out.println("Circle center before moving:");
         c.center.display();
 
-        // move the circle
         c.moveLeft();
         c.moveDown();
 
