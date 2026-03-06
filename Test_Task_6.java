7f12aa642bee78a07ce73fd1f959e9976564bf15
nhajiyev17126 <nhajiyev17126@ada.edu.az>
2026-03-06 09:21:49 +0400

    Changes to task 6 codes

--- Test_Task_6.java
+++ Test_Task_6.java
@@ -2,15 +2,28 @@ public class Test_Task_6 {
 
     public static void main(String[] args) {
 
-        Point p = new Point(2,3);
-        Circle c = new Circle(new Point(5,5), 10);
+        // create a point
+        Point p = new Point(2, 3);
+        System.out.print("Point: ");
+        p.display();
 
+        // move the point
         p.moveUp();
         p.moveRight();
 
+        System.out.print("Point after moving: ");
+        p.display();
+
+        // create a circle
+        Circle c = new Circle(new Point(5, 5), 10);
+        System.out.println("Circle center before moving:");
+        c.center.display();
+
+        // move the circle
         c.moveLeft();
         c.moveDown();
 
-        p.display();
+        System.out.println("Circle center after moving:");
+        c.center.display();
     }
 }
